19821aa0da475e3bf1bbe9e370d217214a2cd986
Hotspring Dev <play.hotspring@gmail.com>
2024-07-04 23:54:02 +0800

    fix

--- ui/status.c
+++ ui/status.c
@@ -178,8 +178,8 @@ void UI_DisplayStatus() {
 //
 //            case 2:		// percentage
 
-        const uint8_t voltage = gBatteryVoltageAverage;
-            sprintf(s, "%u.%02uV", voltage / 1, voltage % 100);
+        const uint16_t voltage = gBatteryVoltageAverage;
+            sprintf(s, "%u.%02uV", voltage / 100, voltage % 100);
         // sprintf(s, "%u%%", BATTERY_VoltsToPercent(gBatteryVoltageAverage));
 //                break;
 //        }
